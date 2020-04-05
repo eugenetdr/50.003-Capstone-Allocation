b99c47694ef8d6ee97e7a06c38481d04497b70d9
Eugene <eugenetdr@gmail.com>
2020-04-05 15:06:52 +0800

    Adds File Upload

--- capstoneAllocation/admin_site/models.py
+++ capstoneAllocation/admin_site/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from datetime import datetime as dt
+import pandas as pd
+import math
 
 # Create your models here.
 
@@ -34,4 +37,165 @@ class Admin(models.Model):
 
 	def logout(self):
 		self.status=0
+		self.save()
+
+class UploadedFiles(models.Model):
+	yearOfGrad = models.IntegerField(default=dt.now().year)
+	fileName=models.CharField(max_length=100)
+	uploadedFile=models.FileField(upload_to='documents/')
+
+	def manageFile(self):
+		if '.csv' in self.fileName:
+			df=pd.DataFrame(pd.read_csv(self.uploadedFile))
+		elif '.xlsx' in self.fileName:
+			df=pd.DataFrame(pd.read_excel(self.uploadedFile))
+		return df
+
+	def updateDimToData(self, df, dim):
+		for i in dim:
+			length=[]
+			width=[]
+			height=[]
+			for j in dim[i]:
+				print(j)
+				dimensionsDict={'Length':0.0, 'Width':0.0, 'Height':0.0}
+				if type(j)==str:
+					stringls=j.split('x')
+					newls=[]
+					for k in stringls:
+						if 'm' in k:
+							newls.append(k.split('m')[0])
+						else:
+							newls.append(k.split(' ')[0])
+
+					try:
+						dimensionsDict['Length']=float(newls[0])
+						dimensionsDict['Width']=float(newls[1])
+						dimensionsDict['Height']=float(newls[2])
+					except:
+						pass
+				length.append(dimensionsDict['Length'])
+				width.append(dimensionsDict['Width'])
+				height.append(dimensionsDict['Height'])
+			toUpdate={(i+'Length'):length,(i+'Width'):width,(i+'Height'):height}
+			df.update(toUpdate)
+		return df
+
+	def convertDfToDB(self, df, year):
+		teamIDls = []
+		yearOfGradls = []
+		projectNamels = []
+		pTypels = []
+		repEmaills = []
+		pDimls = []
+		sDimls = []
+		numPPls = []
+		numBigPedls = []
+		numSmallPedls = []
+		pedDescls = []
+		numMonitorls = []
+		numTVls = []
+		numTablels = []
+		numChairls = []
+		numHDMIls = []
+		otherls = []
+
+		for ind, entry in df.iterrows():
+			if not math.isnan(entry['Exhibit']):
+				teamIDls.append(entry['Exhibit'])
+				yearOfGradls.append(9999)
+				projectNamels.append(entry['Unnamed: 1'])
+				pTypels.append(entry['Unnamed: 3'])
+				repEmaills.append('')
+				numPPls.append(entry['No. of Power Points Needed:'])
+				numBigPedls.append(entry['Pedestal(s):'])
+				numSmallPedls.append(entry['Unnamed: 8'])
+				pedDescls.append(entry['Unnamed: 9'])
+				numMonitorls.append(entry['Other Requests:'])
+				numTVls.append(entry['Unnamed: 11'])
+				numTablels.append(entry['Unnamed: 12'])
+				numChairls.append(entry['Unnamed: 13'])
+				numHDMIls.append(entry['Unnamed: 15'])
+				otherls.append(entry['Unnamed: 17'])
+				pDimls.append(entry['Size and Weight of Physical Prototype:'])
+				sDimls.append(entry['Showcase Space Needed: L x W x H'])
+
+		output = {
+				'teamID':teamIDls,
+				'yearOfGrad':yearOfGradls,
+				'projectName':projectNamels,
+				'pType':pTypels,
+				'repEmail':repEmaills,
+				'pLength':[],
+				'pWidth':[],
+				'pHeight':[],
+				'sLength':[],
+				'sWidth':[],
+				'sHeight':[],
+				'numPP':numPPls,
+				'numBigPed':numBigPedls,
+				'numSmallPed':numSmallPedls,
+				'pedDesc':pedDescls,
+				'numMonitor':numMonitorls,
+				'numTV':numTVls,
+				'numTable':numTablels,
+				'numChair':numChairls,
+				'numHDMI':numHDMIls,
+				'other':otherls,
+				}
+
+		dim = {'p':pDimls, 's':sDimls}
+
+		return output, dim
+
+
+class ReqData(models.Model):
+	teamID = models.CharField(max_length=100)
+	yearOfGrad = models.IntegerField(default=dt.now().year)
+	projectName = models.CharField(max_length=100, null=True)
+	pType = models.CharField(max_length=100, null=True)
+	repEmail = models.EmailField(null=True)
+	pLength = models.FloatField(null=True)
+	pWidth = models.FloatField(null=True)
+	pHeight = models.FloatField(null=True)
+	sLength = models.FloatField(null=True)
+	sWidth = models.FloatField(null=True)
+	sHeight = models.FloatField(null=True)
+	numPP = models.FloatField(null=True)
+	numBigPed = models.FloatField(null=True)
+	numSmallPed = models.FloatField(null=True)
+	pedDesc = models.CharField(max_length=500, null=True)
+	numMonitor = models.FloatField(null=True)
+	numTV = models.FloatField(null=True)
+	numTable = models.FloatField(null=True)
+	numChair = models.FloatField(null=True)
+	numHDMI = models.FloatField(null=True)
+	other = models.CharField(max_length=500, null=True)
+	reqDateTime = models.DateTimeField(null=True)
+
+	def __str__(self):
+		return self.teamID
+
+	def inputDB(self, detailsDict):
+		self.teamID = detailsDict['teamID']
+		self.yearOfGrad = detailsDict['yearOfGrad']
+		self.projectName = detailsDict['projectName']
+		self.pType = detailsDict['pType']
+		self.repEmail = detailsDict['repEmail']
+		self.pLength = detailsDict['pLength']
+		self.pWidth = detailsDict['pWidth']
+		self.pHeight = detailsDict['pHeight']
+		self.sLength = detailsDict['sLength']
+		self.sWidth = detailsDict['sWidth']
+		self.sHeight = detailsDict['sHeight']
+		self.numPP = detailsDict['numPP']
+		self.numBigPed = detailsDict['numBigPed']
+		self.numSmallPed = detailsDict['numSmallPed']
+		self.pedDesc = detailsDict['pedDesc']
+		self.numMonitor = detailsDict['numMonitor']
+		self.numTV = detailsDict['numTV']
+		self.numTable = detailsDict['numTable']
+		self.numChair = detailsDict['numChair']
+		self.numHDMI = detailsDict['numHDMI']
+		self.other = detailsDict['other']
 		self.save()
--- capstoneAllocation/admin_site/views.py
+++ capstoneAllocation/admin_site/views.py
@@ -1,26 +1,33 @@
 # Create your views here.
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Admin
+from .models import Admin, UploadedFiles
 from requirements.models import Team, Request
+from django.core.files.storage import FileSystemStorage
+from django.utils.datastructures import MultiValueDictKeyError
+import sqlite3
+from sqlalchemy import create_engine
+import pandas as pd
+
+db = create_engine('sqlite://')
 
 # Create your views here.
 
-def validate(username, password):
-	status = 0
-	try:
-		Admin.objects.filter(adminID=username).get()
-		admin = Admin.objects.get(adminID=username)
-		if (password == admin.adminPW) & (admin.status==1):
-			status = 2
-		elif (password == admin.adminPW) & (admin.status==0):
-			status = 1
-			admin = Admin.objects.get(adminID=username)
-			admin.status = 1
-			admin.save()
-	except:
-		status=0
-	return status
+# def validate(username, password):
+# 	status = 0
+# 	try:
+# 		Admin.objects.filter(adminID=username).get()
+# 		admin = Admin.objects.get(adminID=username)
+# 		if (password == admin.adminPW) & (admin.status==1):
+# 			status = 2
+# 		elif (password == admin.adminPW) & (admin.status==0):
+# 			status = 1
+# 			admin = Admin.objects.get(adminID=username)
+# 			admin.status = 1
+# 			admin.save()
+# 	except:
+# 		status=0
+# 	return status
 
 def index(request):
 	user = None
@@ -50,6 +57,19 @@ def floorplan(request, active, user):
 	admin = Admin.objects.get(adminID=user)
 	context = {'adminID':user, 'active':active}
 	if (active==admin.status) & (active==1):
+		if request.method == 'POST':
+			try:
+				request.FILES['myfile']
+				myfile = request.FILES['myfile']
+				f=UploadedFiles(yearOfGrad=request.POST["year"], fileName=myfile.name, uploadedFile=myfile)
+				f.save()
+				df=f.manageFile()
+				output, dim = f.convertDfToDB(df, request.POST["year"])
+				df=f.updateDimToData(output, dim)
+				print(pd.read_sql_table(table_name='admin_site_reqdata', con=db))
+				return HttpResponse("File Uploaded")
+			except MultiValueDictKeyError:
+				return render(request, 'admin/floorplan.html', context)
 		return render(request, 'admin/floorplan.html', context)
 	else:
 		return redirect('adminIndex')
